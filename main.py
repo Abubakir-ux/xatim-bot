4d94e3c51ccf20dd3460aef8f1d6d0ae6e7c012d
Abubakir Tohirov <abubakirtohirov720@gmail.com>
2026-05-18 16:04:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -213,7 +213,7 @@ async def cmd_xatim(message: types.Message):
             )
     except:
         pass
-creator_id = user.id
+    creator_id = user.id
     creator_username = f"@{user.username}" if user.username else user.full_name
     data = {
         "creator_id": creator_id,
